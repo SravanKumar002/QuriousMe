9ae9677003feb76081267a0f4d2d511ea6cd35a8
SRAVAN KUMAR EGA <sravankumarega@SRAVANs-MacBook-Pro.local>
2024-05-13 18:22:37 +0530

    ls3

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -3,6 +3,7 @@ import StarRatings from "react-star-ratings";
 import search from "../assets/icons/search.svg";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
 
 const suggested = [
   "figma",
@@ -17,6 +18,7 @@ class SearchResult extends Component {
   state = {
     searchInput: "",
     usersDetailsList: [],
+    loading: true,
   };
 
   onChangeSearchInput = (event) => {
@@ -52,15 +54,19 @@ class SearchResult extends Component {
         console.log(updatedDetails);
         this.setState({
           usersDetailsList: updatedDetails,
+          loading: false,
         });
       }
     } catch (error) {
       console.error("Error fetching data:", error);
+      this.setState({
+        loading: false,
+      });
     }
   };
 
   render() {
-    const { searchInput, usersDetailsList } = this.state;
+    const { searchInput, usersDetailsList, loading } = this.state;
     const searchResults = usersDetailsList.filter((eachUser) =>
       eachUser.name.includes(searchInput)
     );
@@ -68,71 +74,98 @@ class SearchResult extends Component {
     return (
       <>
         <div>
-          <div className="absolute right-[350px] top-[180px] flex gap-3 items-start">
-            <div className="flex items-center my-4 w-[500px] bg-[#D9D9D9] rounded-full px-2 py-1.5">
-              <input
-                type="text"
-                placeholder="Search for expertise"
-                className="outline-none px-3 bg-[#D9D9D9] text-xl w-full text-black"
-                onChange={this.onChangeSearchInput}
+          {/* Loader */}
+          {loading && (
+            <div className="flex flex-col justify-center items-center">
+              <RotatingLines
+                visible={true}
+                height="96"
+                width="96"
+                color="grey"
+                strokeWidth="5"
+                animationDuration="0.75"
+                ariaLabel="rotating-lines-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
               />
-              <div className="flex items-center justify-center bg-black left-shadow rounded-full p-1.5">
-                <img src={search} className="relative h-8 w-8 top-1" alt="" />
-              </div>
             </div>
-          </div>
-          <div className="bg-[#ffffff5c] py-5 px-7 flex gap-10 items-center">
-            <p>Suggested:</p>
-            {suggested.map((e, i) => (
-              <p key={i} className="px-2 py-1.5 border-2 border-yellow-300">
-                {e}
-              </p>
-            ))}
-          </div>
-          <div className="flex justify-between p-10">
-            <div>
-              <h1 className="text-xl">
-                Results for <span className="font-semibold">UI UX Design</span>
-              </h1>
-              <p className="text-[#C1C1C1] text-sm mt-1">250+ results</p>
-            </div>
-          </div>
-          <div className="grid grid-cols-4 mx-7 gap-10">
-            {searchResults.map((user) => (
-              <Link to={`/profile/${user.id}`} key={user.name}>
-                <div>
-                  <img src={user.image} className="" alt="" />
-                  <div className="flex gap-3 items-center">
+          )}
+
+          {/* Content */}
+          {!loading && (
+            <>
+              <div className="absolute right-[350px] top-[180px] flex gap-3 items-start">
+                <div className="flex items-center my-4 w-[500px] bg-[#D9D9D9] rounded-full px-2 py-1.5">
+                  <input
+                    type="text"
+                    placeholder="Search for expertise"
+                    className="outline-none px-3 bg-[#D9D9D9] text-xl w-full text-black"
+                    onChange={this.onChangeSearchInput}
+                  />
+                  <div className="flex items-center justify-center bg-black left-shadow rounded-full p-1.5">
                     <img
-                      src={user.profilePic}
-                      className="h-10 rounded-full my-2"
+                      src={search}
+                      className="relative h-8 w-8 top-1"
                       alt=""
                     />
-                    <p className="font-semibold">{user.name}</p>
                   </div>
-                  <p>{user.desc}</p>
-                  <p className="text-sm text-red-500 font-sans">
-                    {user.expertise}
-                  </p>
-                  <div className="flex gap-3 items-center">
-                    <StarRatings
-                      rating={user.rating}
-                      numberOfStars={5}
-                      starRatedColor="#FFC107"
-                      starDimension="15px"
-                      starSpacing="3px"
-                    />
-                    <p className="relative top-0.5 text-[#FFC107] font-semibold">
-                      exp ({user.experience})
-                    </p>
-                  </div>
-                  <p className="text-lg font-semibold">
-                    Price ₹ {user.startPrice} /-
-                  </p>
                 </div>
-              </Link>
-            ))}
-          </div>
+              </div>
+              <div className="bg-[#ffffff5c] py-5 px-7 flex gap-10 items-center">
+                <p>Suggested:</p>
+                {suggested.map((e, i) => (
+                  <p key={i} className="px-2 py-1.5 border-2 border-yellow-300">
+                    {e}
+                  </p>
+                ))}
+              </div>
+              <div className="flex justify-between p-10">
+                <div>
+                  <h1 className="text-xl">
+                    Results for{" "}
+                    <span className="font-semibold">UI UX Design</span>
+                  </h1>
+                  <p className="text-[#C1C1C1] text-sm mt-1">250+ results</p>
+                </div>
+              </div>
+              <div className="grid grid-cols-4 mx-7 gap-10">
+                {searchResults.map((user) => (
+                  <Link to={`/profile/${user.id}`} key={user.name}>
+                    <div>
+                      <img src={user.image} className="" alt="" />
+                      <div className="flex gap-3 items-center">
+                        <img
+                          src={user.profilePic}
+                          className="h-10 rounded-full my-2"
+                          alt=""
+                        />
+                        <p className="font-semibold">{user.name}</p>
+                      </div>
+                      <p>{user.desc}</p>
+                      <p className="text-sm text-red-500 font-sans">
+                        {user.expertise}
+                      </p>
+                      <div className="flex gap-3 items-center">
+                        <StarRatings
+                          rating={user.rating}
+                          numberOfStars={5}
+                          starRatedColor="#FFC107"
+                          starDimension="15px"
+                          starSpacing="3px"
+                        />
+                        <p className="relative top-0.5 text-[#FFC107] font-semibold">
+                          exp ({user.experience})
+                        </p>
+                      </div>
+                      <p className="text-lg font-semibold">
+                        Price ₹ {user.startPrice} /-
+                      </p>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </>
     );
